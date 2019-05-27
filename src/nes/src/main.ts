e3a55aebf87975b2d797112bdb8ac697ee429ed6
Roger Ngo <urbanspr1nter@gmail.com>
2019-05-27 15:16:54 -0700

    Rendering stuff.

--- src/nes/src/main.ts
+++ src/nes/src/main.ts
@@ -4,41 +4,68 @@ const WIDTH = 256;
 const HEIGHT = 240;
 
 const backCanvas = document.createElement("canvas");
+const backContext = backCanvas.getContext("2d", {
+  alpha: false
+});
 backCanvas.width = WIDTH;
 backCanvas.height = HEIGHT;
-
-const backContext = backCanvas.getContext("2d");
 
 const canvas = document.getElementById("main") as HTMLCanvasElement;
 const context = canvas.getContext("2d");
 
 const nes = new Nes();
 
+const prevBuffer = {
+  buffer: [] as string[][]
+};
+
+const pixelCache: { [id: string]: HTMLCanvasElement } = {};
+
+for (let i = 0; i < HEIGHT; i++) {
+  prevBuffer.buffer[i] = [];
+  for (let j = 0; j < WIDTH; j++) {
+    prevBuffer.buffer[i].push(undefined);
+  }
+}
+
 let currentCycles = 0;
 function drawFrame(frameBuffer: string[][]) {
+  const start = performance.now();
   for (let i = 0; i < HEIGHT; i++) {
     for (let j = 0; j < WIDTH; j++) {
       if (!frameBuffer[i][j]) {
         break;
       }
+
+      if (
+        prevBuffer.buffer[i][j] === undefined &&
+        prevBuffer.buffer[i][j] === frameBuffer[i][j]
+      ) {
+        continue;
+      }
+
+      prevBuffer.buffer[i][j] = frameBuffer[i][j];
+
       backContext.strokeStyle = frameBuffer[i][j];
       backContext.beginPath();
       backContext.moveTo(j, i);
-      backContext.lineTo(j + 1, i + 1);
+      backContext.lineTo(j + 1, i);
       backContext.stroke();
-      backContext.closePath();
     }
+    backContext.closePath();
   }
+
+  console.log(`RENDER TIME: ${performance.now() - start}`);
 }
 
 function renderFrame() {
-  currentCycles += nes.run(7459);
+  currentCycles += nes.run(1865);
   if (currentCycles >= 29833) {
-    const start = performance.now();
     currentCycles = 0;
-    drawFrame(nes.frameBuffer());
-    context.drawImage(backCanvas, 0, 0)
-    console.log(`RENDER TIME: ${performance.now() - start}`);
+    requestAnimationFrame(() => {
+      drawFrame(nes.frameBuffer());
+      context.putImageData(backContext.getImageData(0, 0, 256, 240), 0, 0);
+    });
   }
 }
 
