20e4a431f4b210adaac26fbd2c7b06bd429d2fbf
Roger Ngo <urbanspr1nter@gmail.com>
2019-06-21 19:28:36 -0700

    some refactoring.

--- src/nes/src/main.ts
+++ src/nes/src/main.ts
@@ -1,76 +1,24 @@
 import { Nes, Roms } from "./nes";
-import { UiKeyHandler } from "./ui-key-handler";
+import { UiKeyHandler } from "./ui.keyhandler";
 import { EventEmitter } from "events";
-
-const FPS = 60;
-const TIME_PER_FRAME = Math.ceil(1000 / FPS);
-const LOGICAL_SECOND_INTERVAL = TIME_PER_FRAME * FPS;
-const WIDTH = 256;
-const HEIGHT = 240;
-
-const canvas = document.getElementById("main") as HTMLCanvasElement;
-const context = canvas.getContext("2d", { alpha: false });
-context.imageSmoothingEnabled = false;
+import { UiFrameBuffer } from "./ui.framebuffer";
 
 let nesEventListener = new EventEmitter();
 let nes: Nes;
-let paused = false;
-let consoleShown = false;
 
-const prevBuffer = {
-  buffer: [] as string[][]
-};
-clearPixelBuffer();
-
-function scale(times: number) {
-  canvas.width = times * WIDTH;
-  canvas.height = times * HEIGHT;
-  context.scale(times, times);
-
-  clearPixelBuffer();
-  drawFrame(nes.frameBuffer());
-}
-
-function setupDOM(nes: Nes) {
+function setupDOM(nes: Nes, uiFrameBuffer: UiFrameBuffer) {
   document.getElementById("btn-scale-1").addEventListener("click", () => {
-    scale(1);
+    uiFrameBuffer.scale(1);
   });
   document.getElementById("btn-scale-2").addEventListener("click", () => {
-    scale(2);
+    uiFrameBuffer.scale(2);
   });
   document.getElementById("btn-scale-3").addEventListener("click", () => {
-    scale(3);
+    uiFrameBuffer.scale(3);
   });
   document.getElementById("btn-scale-4").addEventListener("click", () => {
-    scale(4);
+    uiFrameBuffer.scale(4);
   });
-}
-
-function clearPixelBuffer() {
-  for (let i = 0; i < HEIGHT; i++) {
-    if (prevBuffer.buffer[i]) {
-      prevBuffer.buffer[i] = [];
-    } else {
-      prevBuffer.buffer.push([]);
-    }
-    for (let j = 0; j < WIDTH; j++) {
-      prevBuffer.buffer[i].push("");
-    }
-  }
-}
-
-function drawFrame(frameBuffer: string[][]) {
-  for (let i = 0; i < HEIGHT; i++) {
-    for (let j = 0; j < WIDTH; j++) {
-      if (prevBuffer.buffer[i][j] === frameBuffer[i][j]) {
-        continue;
-      }
-
-      prevBuffer.buffer[i][j] = frameBuffer[i][j];
-      context.fillStyle = prevBuffer.buffer[i][j];
-      context.fillRect(j, i, 1, 1);
-    }
-  }
 }
 
 function run() {
@@ -81,6 +29,7 @@ function run() {
 }
 
 document.getElementById("btn-play").addEventListener("click", () => {
+
   const selectElement = (document.getElementById("select-game") as HTMLSelectElement);
   const selectedGame = Number(selectElement.options[selectElement.selectedIndex].value);
 
@@ -88,15 +37,16 @@ document.getElementById("btn-play").addEventListener("click", () => {
 
   nes = new Nes(nesEventListener, game);
 
+  const uiFrameBuffer = new UiFrameBuffer(nes);
+  const uiKeyHandler = new UiKeyHandler(nes.controller1);
+
+  setupDOM(nes, uiFrameBuffer);
+
   nesEventListener.on("renderFrame", () => {
     requestAnimationFrame(() => {
-      drawFrame(nes.frameBuffer());
+      uiFrameBuffer.drawFrame();
     });
   });
-
-  setupDOM(nes);
-
-  const uiKeyHandler = new UiKeyHandler(nes.controller1);
 
   document.addEventListener("keydown", (e: KeyboardEvent) => {
     uiKeyHandler.handlePlayerOneKeyDown(e.key);
---
+++ src/nes/src/ui.framebuffer.ts
@@ -0,0 +1,63 @@
+import { Nes } from "./nes";
+
+const WIDTH = 256;
+const HEIGHT = 240;
+
+export class UiFrameBuffer {
+  private _canvas: HTMLCanvasElement;
+  private _context: CanvasRenderingContext2D;
+  private _prevBuffer: { buffer: string[][] }
+  private _buffer: string[][];
+  private _nes: Nes;
+
+  constructor(nes: Nes) {
+    this._nes = nes;
+
+    this._buffer = [];
+    this._prevBuffer = { buffer: [] };
+    this._clearPixelBuffer();
+
+    this._canvas = document.getElementById("main") as HTMLCanvasElement;
+    this._context = this._canvas.getContext("2d", { alpha: false });
+    this._context.imageSmoothingEnabled = false;
+
+    this.drawFrame();
+  }
+
+  public scale(factor: number) {
+    this._canvas.width = factor * WIDTH;
+    this._canvas.height = factor * HEIGHT;
+    this._context.scale(factor, factor);
+
+    this._clearPixelBuffer();
+    this.drawFrame();
+  }
+
+  public drawFrame() {
+    this._buffer = this._nes.frameBuffer();
+    for (let i = 0; i < HEIGHT; i++) {
+      for (let j = 0; j < WIDTH; j++) {
+        if (this._prevBuffer.buffer[i][j] === this._buffer[i][j]) {
+          continue;
+        }
+
+        this._prevBuffer.buffer[i][j] = this._buffer[i][j];
+        this._context.fillStyle = this._prevBuffer.buffer[i][j];
+        this._context.fillRect(j, i, 1, 1);
+      }
+    }
+  }
+
+  private _clearPixelBuffer() {
+    for (let i = 0; i < HEIGHT; i++) {
+      if (this._prevBuffer.buffer[i]) {
+        this._prevBuffer.buffer[i] = [];
+      } else {
+        this._prevBuffer.buffer.push([]);
+      }
+      for (let j = 0; j < WIDTH; j++) {
+        this._prevBuffer.buffer[i].push("");
+      }
+    }
+  }
+}
