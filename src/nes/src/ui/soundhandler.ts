dbdcc251ed86c357a2f8b445450c8acfed604358
Roger Ngo <urbanspr1nter@gmail.com>
2019-07-14 15:36:12 -0700

    Minor sound buffer handling perf improvements

--- src/nes/src/ui/soundhandler.ts
+++ src/nes/src/ui/soundhandler.ts
@@ -1,6 +1,6 @@
 /**
  * soundhandler.ts
- * 
+ *
  * Roger Ngo, 2019
  */
 import { AUDIO_BUFFER_LENGTH, AUDIO_SAMPLE_RATE } from "../apu/constants";
@@ -12,6 +12,8 @@ export class UiSoundHandler {
   private _buffer: AudioBuffer;
   private _bufferSource: AudioBufferSourceNode;
   private _bufferDataQueue: number[];
+  private _bufferData: Float32Array;
+  private _currIndex: number;
 
   constructor(masterVolume: number) {
     this._audioContext = new AudioContext();
@@ -26,30 +28,34 @@ export class UiSoundHandler {
       AUDIO_BUFFER_LENGTH,
       AUDIO_SAMPLE_RATE
     );
+    this._bufferData = this._buffer.getChannelData(0);
 
     this._bufferDataQueue = [];
+    for (let i = 0; i < AUDIO_BUFFER_LENGTH; i++) {
+      this._bufferDataQueue[i] = 0;
+    }
+    this._currIndex = 0;
   }
 
   /**
-   * Adds the value to the data queue so that it can be outputted to the audio 
-   * device once the buffer queue is full. 
-   * 
+   * Adds the value to the data queue so that it can be outputted to the audio
+   * device once the buffer queue is full.
+   *
    * @param value floating point value representing the sample
    */
   public receiveSample(value: number) {
-    if (this._bufferDataQueue.length >= AUDIO_BUFFER_LENGTH) {
-      const bufferData = this._buffer.getChannelData(0);
-      bufferData.set(this._bufferDataQueue.slice(0, AUDIO_BUFFER_LENGTH));
+    if (this._currIndex === AUDIO_BUFFER_LENGTH) {
+      this._bufferData.set(this._bufferDataQueue);
 
       this._bufferSource = this._audioContext.createBufferSource();
       this._bufferSource.buffer = this._buffer;
       this._bufferSource.connect(this._gainNode);
 
-      this._bufferDataQueue = this._bufferDataQueue.slice(AUDIO_BUFFER_LENGTH);
+      this._currIndex = 0;
 
       this._bufferSource.start();
     }
 
-    this._bufferDataQueue.push(value);
+    this._bufferDataQueue[this._currIndex++] = value;
   }
 }
