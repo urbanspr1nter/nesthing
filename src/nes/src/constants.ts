57e681caeebf977d5d66dd59339e608344e9845f
Roger Ngo <urbanspr1nter@gmail.com>
2019-07-16 19:47:55 -0700

    Nametable mirroring

--- src/nes/src/constants.ts
+++ src/nes/src/constants.ts
@@ -3,9 +3,9 @@ export const CpuFrequencyHz = 1789773;
 export const CpuCyclesInFrame = 29780;
 
 export enum MirrorMode {
+  MirrorHorizontal,
+  MirrorVertical,
   MirrorSingle0,
   MirrorSingle1,
-  MirrorVertical,
-  MirrorHorizontal,
   MirrorFour
 }
