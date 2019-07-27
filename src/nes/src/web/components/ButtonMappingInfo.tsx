7aff57ad3c95a05bed572d65f7136ebec97416f2
Roger Ngo <urbanspr1nter@gmail.com>
2019-07-27 11:04:48 -0700

    Player 2 Button Mapping info

--- src/nes/src/web/components/ButtonMappingInfo.tsx
+++ src/nes/src/web/components/ButtonMappingInfo.tsx
@@ -3,14 +3,14 @@ import * as React from "react";
 require("./ButtonMappingInfo.css");
 
 const styles = {
-    container: {
-        marginBottom: 8,
-        display: "inline-flex",
-    } as React.CSSProperties,
-    table: {
-        border: "1px solid black",
-        borderCollapse: "collapse"
-    } as React.CSSProperties
+  container: {
+    marginBottom: 8,
+    display: "inline-flex"
+  } as React.CSSProperties,
+  table: {
+    border: "1px solid black",
+    borderCollapse: "collapse"
+  } as React.CSSProperties
 };
 
 export const ButtonMappingInfo: React.FunctionComponent = () => {
@@ -36,27 +36,65 @@ export const ButtonMappingInfo: React.FunctionComponent = () => {
             <td>J, K</td>
           </tr>
         </table>
+        <strong>Player 2: Keyboard Mapping</strong>
+        <table className="button-mapping-info-table" style={styles.table}>
+          <tr>
+            <td>START</td>
+            <td>/</td>
+          </tr>
+          <tr>
+            <td>SELECT</td>
+            <td>.</td>
+          </tr>
+          <tr>
+            <td>A, B</td>
+            <td>m, , </td>
+          </tr>
+          <tr>
+            <td>Up, Left, Down, Right</td>
+            <td>ArrowUp, ArrowLeft, ArrowDown, ArrowRight</td>
+          </tr>
+        </table>
       </div>
       <div>
-          <strong>Player 1: XBOX 360 Controller Mapping</strong>
-          <table className="button-mapping-info-table">
-              <tr>
-                  <td>START</td>
-                  <td>Start</td>
-              </tr>
-              <tr>
-                  <td>SELECT</td>
-                  <td>Select</td>
-              </tr>
-              <tr>
-                  <td>Up, Left, Down, Right</td>
-                  <td>DPad-Up, DPad-Left, DPad-Down, DPad-Right</td>
-              </tr>
-              <tr>
-                  <td>A, B</td>
-                  <td>A, X</td>
-              </tr>
-          </table>
+        <strong>Player 1: XBOX 360 Controller Mapping</strong>
+        <table className="button-mapping-info-table">
+          <tr>
+            <td>START</td>
+            <td>Start</td>
+          </tr>
+          <tr>
+            <td>SELECT</td>
+            <td>Select</td>
+          </tr>
+          <tr>
+            <td>Up, Left, Down, Right</td>
+            <td>DPad-Up, DPad-Left, DPad-Down, DPad-Right</td>
+          </tr>
+          <tr>
+            <td>A, B</td>
+            <td>A, X</td>
+          </tr>
+        </table>
+        <strong>Player 2: XBOX 360 Controller Mapping</strong>
+        <table className="button-mapping-info-table">
+          <tr>
+            <td>START</td>
+            <td>Start</td>
+          </tr>
+          <tr>
+            <td>SELECT</td>
+            <td>Select</td>
+          </tr>
+          <tr>
+            <td>Up, Left, Down, Right</td>
+            <td>DPad-Up, DPad-Left, DPad-Down, DPad-Right</td>
+          </tr>
+          <tr>
+            <td>A, B</td>
+            <td>A, X</td>
+          </tr>
+        </table>
       </div>
     </div>
   );
