8242708f8941a1f95905ddc52f628b7e2b51e890
Roger Ngo <urbanspr1nter@gmail.com>
2019-08-01 05:35:53 -0700

    Experimenting with WebRTC using peerjs for network play.

--- src/nes/src/main.ts
+++ src/nes/src/main.ts
@@ -1,6 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./web/components/App";
+import Peer from "peerjs";
+import * as uuid from "uuid/v4";
 
 // @ts-ignore
 const WasmModule = Module;
@@ -8,10 +10,39 @@ const WasmModule = Module;
 // Run bootstrap code
 checkModule();
 
+const thisPeerId = uuid();
+console.log("The peer ID is: ", thisPeerId);
+
+const peer = new Peer(thisPeerId);
+peer.on("open", function(id:string) {
+  console.log("The peer ID is: ", id);
+});
+peer.on("connection", function(connection) {
+  console.log(connection);
+  connection.on("data", function(data) {
+    console.log("Data received!", data);
+  });
+});
+
 function init() {
   ReactDOM.render(React.createElement(App, {wasmModule: WasmModule}), document.getElementById("app"));
 
   document.getElementById("overlay").className = "hidden";
+
+
+
+  document.getElementById("peer-id-connect").addEventListener("click", () => {
+    const peerId = (document.getElementById("peer-id") as HTMLInputElement).value;
+
+    console.log(peerId);
+
+    var connection = peer.connect(peerId);
+    connection.on("open", function() {
+      document.getElementById("peer-send").addEventListener("click", () => {
+        connection.send(`Random ${Math.random()} from ${thisPeerId}`);
+      });
+    });
+  });
 }
 
 function checkModule() {
