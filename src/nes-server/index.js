656e03fe356f4eef098b2bd03ad741f4e157867e
Roger Ngo <urbanspr1nter@gmail.com>
2019-08-04 14:45:37 -0700

    Socket IO stuff.

--- src/nes-server/index.js
+++ src/nes-server/index.js
@@ -1,8 +1,17 @@
 const app = require("express")();
 const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
     res.status(200).send("It works!");
+});
+
+io.on("connection", (socket) => {
+    console.log("User connected");
+    socket.on("ping", (name, fn) => {
+        console.log("ping!");
+        fn("pong");
+    });
 });
 
 http.listen(3000, () => {
--- src/nes/src/main.ts
+++ src/nes/src/main.ts
@@ -2,6 +2,12 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./web/components/App";
 import NetPlay from "./netplay";
+import io from "socket.io-client";
+
+const SERVER_ENDPOINT = "http://localhost:3000/";
+
+const socket = io(SERVER_ENDPOINT);
+// socket.connect();
 
 // @ts-ignore
 const WasmModule = Module;
@@ -12,6 +18,8 @@ checkModule();
 
 function init() {
   const netplay = new NetPlay();
+
+  socket.emit("ping", "ping", data => console.log(data));
 
   ReactDOM.render(
     React.createElement(App, { wasmModule: WasmModule, netplay: netplay }),
